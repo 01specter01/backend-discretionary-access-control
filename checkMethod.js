7656676171d643dca880f86ec82114313ab6517a
Leo Volwahsen <volwahsenl@gmail.com>
2023-01-31 18:49:35 +0100

    double files

--- checkMethod.js
+++
@@ -1,8 +0,0 @@
-function checkMethod(req, res, next) {
-    if (!["GET", "POST", "PUT", "DELETE"].includes(req.method)) {
-        return res.status(405).end();
-    }
-    next();
-}
-
-export default checkMethod;
--- checkRole.js
+++
@@ -1,9 +0,0 @@
-function checkRole(roles) {
-    return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
-            return req.status(403).end();
-        }
-        next();
-    };
-}
-export default checkRole;
